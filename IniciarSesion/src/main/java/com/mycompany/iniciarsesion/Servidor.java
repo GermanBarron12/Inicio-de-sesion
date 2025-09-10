791c6c0b8aff95f1ed0f5dd57e98f1d779c83672
German Barron <germanbarrona12@gmail.com>
2025-09-09 21:02:11 -0700

    Primer commit, Subiendo proyecto de netbeans

---
+++ IniciarSesion/src/main/java/com/mycompany/iniciarsesion/Servidor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iniciarsesion;
+
+/**
+ *
+ * @author gerth
+ */
+public class Servidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
