fc4cf42f02b28c2b937a64bfa5e24ace40346266
German Barron <germanbarrona12@gmail.com>
2025-09-09 21:48:45 -0700

    escribi el cliente basico para que se conecte al servidor

--- IniciarSesion/src/main/java/com/mycompany/iniciarsesion/Cliente.java
+++ IniciarSesion/src/main/java/com/mycompany/iniciarsesion/Cliente.java
@@ -1,10 +1,19 @@
 
 package com.mycompany.iniciarsesion;
 
-/**
- *
- * @author gerth
- */
+import java.io.IOException;
+import java.net.Socket;
+
+
 public class Cliente {
-    
+    public static void main(String[] args) throws IOException{
+        String host = "localhost";
+        int puerto = 5000;
+        
+        try(Socket socket = new Socket(host, puerto)){
+            System.out.println("Conectado al servidor.");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
